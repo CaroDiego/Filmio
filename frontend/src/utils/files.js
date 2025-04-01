e54faf64dc29a7c0a6e7bae3ff3a92173d01f6b3
CaroDiego <dicafre1404@gmail.com>
2025-04-01 11:24:27 +0200

    redo: formatBytes

--- frontend/src/utils/files.js
+++ frontend/src/utils/files.js
@@ -1,8 +1,23 @@
-export const formatBytes = (bytes, decimals) => {
+/**
+ * Converts a given number of bytes into a human-readable string format.
+ *
+ * @param {number} bytes - The number of bytes to format.
+ * @param {number} [decimals=2] - The number of decimal places to include in the formatted output.
+ * @returns {string} A string representing the formatted size in appropriate units (e.g., "KB", "MB").
+ *
+ * @example
+ * formatBytes(1024); // "1.00 KB"
+ * formatBytes(123456789, 3); // "117.738 MB"
+ */
+export const formatBytes = (bytes, decimals = 2) => {
   if (bytes == 0) return "0 Bytes";
-  const k = 1024,
-    dm = decimals || 2,
-    sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"],
-    i = Math.floor(Math.log(bytes) / Math.log(k));
-  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
+
+  const k = 1024;
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+  const i = Math.floor(Math.log(bytes) / Math.log(k)); //Determine which size gets
+  const size = parseFloat((bytes / Math.pow(k, i)).toFixed(decimals)); //Convert the bytes to the correspondent unit
+
+  return size + " " + sizes[i]; // Concatenate the size with his unit
 };
+
+console.log(formatBytes(50802));
