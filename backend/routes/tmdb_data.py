91b547aeae9b730b0477e0a2069c3bf9aabbc259
CaroDiego <dicafre1404@gmail.com>
2025-07-04 15:32:10 +0200

    feat: do moviefetcher more efficient

--- backend/routes/tmdb_data.py
+++ backend/routes/tmdb_data.py
@@ -1,8 +1,12 @@
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
 import json
 from fastapi import APIRouter
+import httpx
 import pandas as pd
 
 from tmdb_data.get_info import get_movie_list
+from utils.file import validate_path
 
 
 tmdb_data_router = APIRouter()
@@ -10,17 +14,22 @@ tmdb_data_router = APIRouter()
 @tmdb_data_router.get("/tmdbdata")
 async def tmdb_data():
     
+    file_path = "temp/letterboxd_data/final.json"
+    validate_path(file_path)
+
     try:
-        file_path = "temp/letterboxd_data/final.json"
         df = pd.read_json(file_path)
 
-        results = []
-        for _, row in df.iterrows():
-            name = row.get('Name')
-            year = row.get('Year')
-            movie_info = get_movie_list(name=name, year=year)
-            results.append(movie_info)
+        def fetch_movie(row):
+            name = getattr(row, 'Name', None)
+            year = getattr(row, 'Year', None)
+            return get_movie_list(name=name, year=year)
+        
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            results = list(executor.map(fetch_movie, df.itertuples(index=False)))
+            
         return results
-
+        
+            
     except Exception as e:
         return {"error": str(e)}
